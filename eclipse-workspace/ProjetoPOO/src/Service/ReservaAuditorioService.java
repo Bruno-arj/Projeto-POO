353310614102fdc7b89b29b38672b530cbfda653
kaua-L1 <kaua6974@gmail.com>
2025-12-06 10:02:34 -0300

    Criação do Service

---
+++ eclipse-workspace/ProjetoPOO/src/Service/ReservaAuditorioService.java
@@ -0,0 +1,52 @@
+package Service;
+import Model.Auditorio;
+import java.util.ArrayList;
+import Model.Reserva;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ReservaAuditorioService {
+	ArrayList<Reserva> listaAuditorio = new ArrayList<Reserva>();
+	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+	
+	public String ReservaAuditorio(int id, String nome, int capacidade, boolean disponivel,
+										  String dataInicio, String dataFim) {
+		LocalDateTime inicio = ConverterData(dataInicio);
+		LocalDateTime fim = ConverterData(dataFim);
+		if (fim.isBefore(inicio)) {
+			return "Erro throws quando der certo coloca pra retorna o preco";
+		}
+		else if(inicio.equals(fim)) {
+			return "Erro throws quando der certo coloca pra retorna preco";
+		}
+		Auditorio auditorio = new Auditorio(id,nome,capacidade,disponivel);
+		Duration duracao = Duration.between(inicio, fim);
+		long horas = duracao.toHours();
+		double custo = auditorio.calcularCustoReserva(horas);
+		
+		Reserva reserva = new Reserva(id, auditorio, inicio, fim, custo,false, 0.2);
+		
+		AdicionarListaAuditorio(reserva);
+		return "Reserva bem sucedida";
+	}
+	
+	public LocalDateTime ConverterData(String data) {
+		return LocalDateTime.parse(data, formato);
+	}
+
+	private void AdicionarListaAuditorio(Reserva reserva) {
+		listaAuditorio.add(reserva);
+	}
+
+	public ArrayList<Reserva> getListaAuditorio() {
+		return listaAuditorio;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservaAuditorioService [listaAuditorio=" + listaAuditorio + "]";
+	}
+	
+	
+}
---
+++ eclipse-workspace/ProjetoPOO/src/Service/ReservaCabineIndividualService.java
@@ -0,0 +1,53 @@
+package Service;
+import Model.CabineIndividual;
+
+import Model.Reserva;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class ReservaCabineIndividualService {
+	ArrayList<Reserva> listaIndividual = new ArrayList<Reserva>();
+	DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+	
+	public String ReservaCabine(int id, String nome, int capacidade, boolean disponivel, 
+										  String dataInicio, String dataFim) {
+		LocalDateTime inicio = ConverterData(dataInicio);
+		LocalDateTime fim = ConverterData(dataFim);
+		if (fim.isBefore(inicio)) {
+			return "Erro throws quando der certo coloca pra retorna o preco";
+		}
+		else if(inicio.equals(fim)) {
+			return "Erro throws quando der certo coloca pra retorna preco";
+		}
+		CabineIndividual cabineIndividual = new CabineIndividual(id,nome,capacidade,disponivel);
+		Duration duracao = Duration.between(inicio, fim);
+		long horas = duracao.toHours();
+		double custo = cabineIndividual.calcularCustoReserva(horas);
+		
+		Reserva reserva = new Reserva(id, cabineIndividual, inicio, fim, custo,false, 0.2);
+		
+		AdicionarListaCabine(reserva);
+		return "Reserva bem sucedida";
+	}
+	
+	public LocalDateTime ConverterData(String data) {
+		return LocalDateTime.parse(data, formato);
+	}
+	
+	private void AdicionarListaCabine(Reserva reservaCabineIndividual) {
+		listaIndividual.add(reservaCabineIndividual);
+	}
+
+	public ArrayList<Reserva> getListaIndividual() {
+		return listaIndividual;
+	}
+
+	@Override
+	public String toString() {
+		return "ReservaCabineIndividualService [listaIndividual=" + listaIndividual + "]";
+	}
+	
+}
