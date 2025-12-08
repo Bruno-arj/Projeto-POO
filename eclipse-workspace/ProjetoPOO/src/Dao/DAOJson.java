8c50349017b4d7b70f624f791313ff98ca235c5f
Bruno Araujo <brunoaraujosume@gmail.com>
2025-12-08 11:28:12 -0300

    att apagando adapter

--- eclipse-workspace/ProjetoPOO/src/Dao/DAOJson.java
+++ eclipse-workspace/ProjetoPOO/src/Dao/DAOJson.java
@@ -1,6 +1,5 @@
 package Dao;
 
-import Model.Espaco;
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -20,7 +19,6 @@ public abstract class DAOJson<T> implements Persistencia<T> {
     private final Gson gson = new GsonBuilder()
             .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
             .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
-            .registerTypeAdapter(Espaco.class, new EspacoAdapter()) // <--- ADICIONE ESTA LINHA
             .setPrettyPrinting()
             .create();
 
